91a981a68ce4b4661566088e06a6220ddc934a22
bjaimes3 <54960453+bjaimes3@users.noreply.github.com>
2019-10-06 19:07:20 -0700

    Add files via upload

---
+++ AddingMachine.java
@@ -0,0 +1,53 @@
+package cse360assign2;
+
+
+/**
+ * Class method used AddingMachine contains methods getTotal(), add(), subtract(), toString(), and clear()
+ */
+public class AddingMachine {
+
+	private int total;
+
+
+	/**
+	 * Cunstructor class for AddingMachine
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+
+	/**
+	 * method that returns an integer does not take input
+	 */
+	public int getTotal () {
+		return 0;
+	}
+
+	/**
+	 * method that take an integer, and adds it to the sum and adds it to toString
+	 */
+	public void add (int value) {
+		
+	}
+
+	/**
+	 * method that takes an integer, and subtracts it from the sum and adds the integer to toString
+	 */
+	public void subtract (int value) {
+		
+	}
+
+	/**
+	 * method that returns a string that contains that logged entires from user
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * clears the string
+	 */
+	public void clear() {
+	
+	}
+}
